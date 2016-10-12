1237a152fd129b0ba328f93e254037a4a5991c68
Andrian Ivanin <andrian.ivanin@gmail.com>
2016-10-11 22:31:57 -0400

    F-3 Participant CRUD

---
+++ myReaServer-v1-Passport/models/participants.js
@@ -0,0 +1,44 @@
+// grab the things we need
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var participantSchema = new Schema({
+    firstName:  {
+        type: String,
+        required: true
+    },
+    lastName:  {
+        type: String,
+        required: true
+    }, 
+    role:  {
+        type: String,
+        required: true
+    },
+    roleDescription:  {
+        type: String,
+        required: true
+    },      
+    phone:  {
+        type: String,
+        required: true
+    },    
+    text:  {
+        type: String,
+        required: true
+    }, 
+    email:  {
+        type: String,
+        required: true
+    }    
+}, {
+    timestamps: true
+});
+
+
+// the schema is useless so far
+// we need to create a model using it
+var participants = mongoose.model('participant', participantSchema);
+
+// make this available to our Node applications
+module.exports = participants;
